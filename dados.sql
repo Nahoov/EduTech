80283e298b0114fd43e8e19f0496eab627e20df6
Nahoov <newnahomi15@gmail.com>
2025-10-14 00:38:33 -0300

    insert de duas tabelas

--- dados.sql
+++ dados.sql
@@ -1,1 +1,16 @@
 -- Script SQL com os INSERTs de dados
+INSERT INTO categoria (nome_categoria) VALUES
+('Front-end'),
+('Back-end'),
+('DevOps'),
+('Data Sciece'),
+('CiberSecurity');
+
+INSERT INTO especialidades (nome_especialidade) VALUES
+('Desenvolvedor JAVA'),
+('Desenvolvedor PYTHON'),
+('Desenvolvedor web'),
+('Engenheiro de dados'),
+('Analista de arquitetura de software'),
+('Desenvolvedor CLOUD AWS'),
+('Especialista em cibersegurança');
